c829610a54e04b9a486b582b9b99e42ae88202da
NormanTheManlyMan <63070822+NormanTheManlyMan@users.noreply.github.com>
2021-09-05 16:43:51 -0700

    Implemented Message Handling and Painting to the Window

--- code/win32_platformer.cpp
+++ code/win32_platformer.cpp
@@ -1,11 +1,124 @@
 #include <windows.h> 
 
-int CALLBACK 
-WinMain(HINSTANCE hInstance, 
-        HINSTANCE hPrevInstance,
-        PSTR lpCmdLine, 
-        INT nCmdShow)
+LRESULT CALLBACK 
+MainWindowCallback(
+                   HWND   Window,
+                   UINT   Message,
+                   WPARAM WParam,
+                   LPARAM LParam
+                   )
 {
-    MessageBoxA(0, "Running", "Simple-Platformer", MB_OK|MB_ICONINFORMATION);
+    LRESULT Result = 0;
+    
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+        
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+        
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+            
+        } break;
+        
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+        
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            static DWORD Operation = WHITENESS;
+            PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+            
+            if (Operation == WHITENESS)
+            {
+                Operation = BLACKNESS;
+            }
+            else
+            {
+                Operation = WHITENESS;
+            }
+            EndPaint(Window, &Paint);
+        } break;
+        
+        default:
+        {
+            //OutputDebugStringA("DEFAULT\n");
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+    
+    return Result;
+}
+
+int CALLBACK 
+WinMain(HINSTANCE Instance, 
+        HINSTANCE PrevInstance,
+        PSTR CommandLine, 
+        INT ShowCode)
+{
+    WNDCLASS WindowClass = {}; //set the window class to 0
+    
+    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW; 
+    WindowClass.lpfnWndProc = MainWindowCallback; //specify procedure that handles windows messages
+    WindowClass.hInstance = Instance;
+    WindowClass.lpszClassName = "PlatformerWindowClass";
+    
+    if(RegisterClass(&WindowClass))
+    {
+        HWND WindowHandle = CreateWindowEx(0,
+                                           WindowClass.lpszClassName,
+                                           "Simple Platformer",
+                                           WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                                           CW_USEDEFAULT,
+                                           CW_USEDEFAULT,
+                                           CW_USEDEFAULT,
+                                           CW_USEDEFAULT,
+                                           0,
+                                           0,
+                                           Instance,
+                                           0);
+        if(WindowHandle)
+        {
+            for(;;)
+            {
+                MSG Message;
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0)
+                {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            
+        }
+        else
+        {
+            //TODO: loggin
+        }
+    } 
+    else
+    {
+    }
+    
     return 0;
 }
