1715f1d250f69054cc953b2e1a50b6e9b9291e2b
NormanTheManlyMan <63070822+NormanTheManlyMan@users.noreply.github.com>
2021-09-08 20:23:25 -0700

    Create bitmap and back buffer

--- code/win32_platformer.cpp
+++ code/win32_platformer.cpp
@@ -1,31 +1,131 @@
 #include <windows.h> 
+#include <stdint.h>
+
+#define internal static 
+#define local_persist static  
+#define global_variable static
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+
+global_variable bool Running;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+global_variable int BytesPerPixel = 4;
+
+internal void 
+RenderWeirdGradient(int XOffset, int YOffset)
+{
+    int Width = BitmapWidth;
+    int Height = BitmapHeight; 
+    
+    int Pitch = Width*BytesPerPixel;
+    uint8 *Row = (uint8 *)BitmapMemory;
+    for(int Y = 0;
+        Y < BitmapHeight;
+        ++Y)
+    {
+        uint32 *Pixel = (uint32 *)Row;
+        for(int X = 0;
+            X < BitmapWidth;
+            ++X)
+        {
+            uint8 Blue = (X + XOffset);
+            uint8 Green = (Y + YOffset);
+            
+            *Pixel++ = ((Green << 8) | Blue);
+            
+            
+        }
+        
+        Row += Pitch;
+    }
+}
+
+internal void 
+Win32ResizeDIBSection(int Width, int Height)
+{
+    if(BitmapMemory)
+    {
+        VirtualFree(BitmapMemory, 0, MEM_RELEASE);
+    }
+    
+    BitmapWidth = Width;
+    BitmapHeight = Height;
+    
+    
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader); 
+    BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+    BitmapInfo.bmiHeader.biHeight = -BitmapHeight;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    
+    
+    
+    
+    int BitmapMemorySize = (BitmapWidth * BitmapHeight) * BytesPerPixel;
+    BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+    
+}
+
+internal void 
+Win32UpdateWindow(HDC DeviceContext, RECT *ClientRect, int X, int Y, int Width, int Height)
+{
+    int WindowWidth = ClientRect->right - ClientRect->left;
+    int WindowHeight = ClientRect->bottom - ClientRect->top;
+    StretchDIBits(DeviceContext,
+                  //X, Y, Width, Height,
+                  //X, Y, Width, Height,
+                  0, 0, BitmapWidth, BitmapHeight,
+                  0, 0, BitmapWidth, BitmapHeight,
+                  BitmapMemory, &BitmapInfo,
+                  DIB_RGB_COLORS, SRCCOPY);
+}
 
 LRESULT CALLBACK 
-MainWindowCallback(
-                   HWND   Window,
-                   UINT   Message,
-                   WPARAM WParam,
-                   LPARAM LParam
-                   )
+Win32MainWindowCallback(HWND   Window,
+                        UINT   Message,
+                        WPARAM WParam,
+                        LPARAM LParam)
 {
     LRESULT Result = 0;
     
     switch(Message)
     {
+        
         case WM_SIZE:
         {
-            OutputDebugStringA("WM_SIZE\n");
-        } break;
-        
-        case WM_DESTROY:
-        {
-            OutputDebugStringA("WM_DESTROY\n");
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Height = ClientRect.bottom - ClientRect.top;
+            int Width = ClientRect.right - ClientRect.left;
+            Win32ResizeDIBSection(Width, Height);
+            
         } break;
         
         case WM_CLOSE:
         {
-            OutputDebugStringA("WM_CLOSE\n");
+            //TODO: Handle with message
+            Running = false;
             
+        } break;
+        
+        case WM_DESTROY:
+        {
+            //TODO: Handle with error 
+            Running = false;
         } break;
         
         case WM_ACTIVATEAPP:
@@ -41,17 +141,11 @@ MainWindowCallback(
             int Y = Paint.rcPaint.top;
             int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
             int Width = Paint.rcPaint.right - Paint.rcPaint.left;
-            static DWORD Operation = WHITENESS;
-            PatBlt(DeviceContext, X, Y, Width, Height, Operation);
             
-            if (Operation == WHITENESS)
-            {
-                Operation = BLACKNESS;
-            }
-            else
-            {
-                Operation = WHITENESS;
-            }
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            
+            Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
             EndPaint(Window, &Paint);
         } break;
         
@@ -74,39 +168,58 @@ WinMain(HINSTANCE Instance,
     WNDCLASS WindowClass = {}; //set the window class to 0
     
     WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW; 
-    WindowClass.lpfnWndProc = MainWindowCallback; //specify procedure that handles windows messages
+    WindowClass.lpfnWndProc = Win32MainWindowCallback; //specify procedure that handles windows messages
     WindowClass.hInstance = Instance;
     WindowClass.lpszClassName = "PlatformerWindowClass";
     
     if(RegisterClass(&WindowClass))
     {
-        HWND WindowHandle = CreateWindowEx(0,
-                                           WindowClass.lpszClassName,
-                                           "Simple Platformer",
-                                           WS_OVERLAPPEDWINDOW|WS_VISIBLE,
-                                           CW_USEDEFAULT,
-                                           CW_USEDEFAULT,
-                                           CW_USEDEFAULT,
-                                           CW_USEDEFAULT,
-                                           0,
-                                           0,
-                                           Instance,
-                                           0);
-        if(WindowHandle)
+        HWND Window = CreateWindowEx(0,
+                                     WindowClass.lpszClassName,
+                                     "Simple Platformer",
+                                     WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                                     CW_USEDEFAULT,
+                                     CW_USEDEFAULT,
+                                     CW_USEDEFAULT,
+                                     CW_USEDEFAULT,
+                                     0,
+                                     0,
+                                     Instance,
+                                     0);
+        if(Window)
         {
-            for(;;)
+            int XOffset = 0;
+            int YOffset = 0;
+            Running = true;
+            while(Running)
             {
+                
                 MSG Message;
-                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
-                if (MessageResult > 0)
+                while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
                 {
+                    if (Message.message == WM_QUIT)
+                    {
+                        Running = false;
+                    }
                     TranslateMessage(&Message);
                     DispatchMessage(&Message);
+                    
+                    
+                    
+                    
                 }
-                else
-                {
-                    break;
-                }
+                
+                RenderWeirdGradient(XOffset, YOffset);
+                
+                HDC DeviceContext = GetDC(Window);
+                RECT ClientRect;
+                GetClientRect(Window, &ClientRect);
+                int WindowWidth = ClientRect.right - ClientRect.left;
+                int WindowHeight = ClientRect.bottom - ClientRect.top;
+                Win32UpdateWindow(DeviceContext, &ClientRect, 0, 0, WindowWidth, WindowHeight);
+                ReleaseDC(Window, DeviceContext);
+                
+                YOffset += 5;
             }
             
             
